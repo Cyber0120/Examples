d5ec7ae67af717f6130a821a4e5cbd3695182419
Cyber0120 <sait574577@gmail.com>
2021-07-06 22:12:36 +0300

    javascrpit

---
+++ 6/6.js
@@ -0,0 +1,8 @@
+function test3(){
+    document.getElementById("test3").innerHTML = "Merhaba JavaScript";
+}
+
+function test4(){
+    document.getElementById("test4").innerHTML = "Merhaba JavaScript";
+    document.getElementById("test4").style.color = "red";
+}
